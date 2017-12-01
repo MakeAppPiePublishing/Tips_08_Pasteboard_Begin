7cd31b03a609d6e4aad2f976296fcae87aa92c2c
Steve Lipton <steve@makeapppie.com>
2017-12-01 10:06:26 -0600

    Added files via Upload

---
+++ MiniTyper.xcodeproj/MiniTyper/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MiniTyper
+//
+//  Created by Steven Lipton on 11/27/17.
+//  Copyright © 2017 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var typingText: UITextView!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+   
+    @IBAction func copyText(_ sender: UIButton) {
+    }
+    
+    @IBAction func pasteText(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        typingText.becomeFirstResponder()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
